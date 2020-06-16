8ebbdd1a9580b4f47a543f59b40adb8bda1d4bd0
johkli <johannes.klint@etraveligroup.com>
2020-06-16 10:36:42 +0200

    Add config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/*eslint-env node*/
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/prop-types": 0,
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+  },
+}
