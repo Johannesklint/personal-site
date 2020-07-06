b80aa2e4db3bcc9c2859a59dd942b9cf2e724dcb
johkli <johannes.klint@etraveligroup.com>
2020-07-06 12:53:22 +0200

    Refactor isAuthenticated hook

--- pages/admin.js
+++ pages/admin.js
@@ -1,19 +1,41 @@
-import React, { useState, useEffect } from "react"
+/* eslint-disable indent */
+import React, { useState, useEffect, useReducer } from "react"
 import firebase from "../src/utils/firebaseConfig"
 import Home from "."
 import Login from "../src/login"
 import Loading from "../src/loading"
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "isAuth":
+      return { ...state, isLoggedIn: true, isLoading: false }
+    case "isNotAuth":
+      return { ...state, isLoggedIn: false, isLoading: false }
+    case "loginError":
+      return {
+        ...state,
+        isLoggedIn: false,
+        hasError: "Either email or password is wrong…",
+      }
+    default:
+      throw new Error()
+  }
+}
+
+const initState = {
+  isLoggedIn: false,
+  hasError: false,
+  isLoading: true,
+}
 function useAuthentication() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [hasError, setHasError] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
+  const [state, dispatch] = useReducer(reducer, initState)
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true)
-        setIsLoading(false)
+        dispatch({ type: "isAuth" })
+      } else {
+        dispatch({ type: "isNotAuth" })
       }
     })
   }, [])
@@ -25,19 +47,23 @@ function useAuthentication() {
         .auth()
         .signInWithEmailAndPassword(email, password)
         .catch(() => {
-          setHasError("Either email or password is wrong…")
-          setIsLoggedIn(false)
+          dispatch({ type: "loginError" })
         })
     }
   }
 
-  return [isLoggedIn, handleLogin, { error: hasError, loading: isLoading }]
+  return {
+    isLoggedIn: state.isLoggedIn,
+    handleLogin,
+    error: state.hasError,
+    loading: state.isLoading,
+  }
 }
 
 export default function Admin({ text, image }) {
   const [hasClickedTextArea, setHasClickedTextArea] = useState(false)
   const [textValue, setTextvalue] = useState("")
-  const [isLoggedIn, handleLogin, { loading, error }] = useAuthentication()
+  const { isLoggedIn, handleLogin, loading, error } = useAuthentication()
 
   function writeText() {
     firebase.database().ref("content/").update({
@@ -99,18 +125,10 @@ export async function getStaticProps() {
     .once("value")
     .then((res) => res.val())
 
-  // let isAuth = false
-  // firebase.auth().onAuthStateChanged((user) => {
-  //   if (user) {
-  //     isAuth = true
-  //   }
-  // })
-
   return {
     props: {
       text: text.replace(/['"]+/g, ""),
       image,
-      // isAuth,
     },
   }
 }
